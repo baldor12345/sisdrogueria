78977583eac0d9968acc46bed9417f2bd3aca779
alex_gastelo <alexgastelob@outlook.com>
2019-03-30 15:22:44 -0500

    GASTELO

--- database/migrations/2019_03_29_233444_crear_tabla_person.php
+++ database/migrations/2019_03_29_233444_crear_tabla_person.php
@@ -19,7 +19,6 @@ Schema::create('person', function (Blueprint $table) {
     $table->increments('id');
     $table->string('nombres',100)->nullable();
     $table->string('apellidos',100)->nullable();
- 
     $table->char('dni',8)->nullable();
     $table->char('ruc',11)->nullable();
     $table->string('direccion',120)->nullable();
@@ -31,8 +30,8 @@ Schema::create('person', function (Blueprint $table) {
     $table->char('estado',1)->nullable();//A=Activo, I=Inactivo
     $table->integer('tipo_persona_id')->unsigned()->nullable();
     $table->foreign('tipo_persona_id')->references('id')->on('tipo_persona')->onUpdate('restrict')->onDelete('restrict');
-    $table->integer('sucursal_id')->unsigned()->nullable();
-    $table->foreign('sucursal_id')->references('id')->on('sucursal')->onUpdate('restrict')->onDelete('restrict');
+    // $table->integer('sucursal_id')->unsigned()->nullable(); 
+    // $table->foreign('sucursal_id')->references('id')->on('sucursal')->onUpdate('restrict')->onDelete('restrict');
     $table->timestamps();
     $table->softDeletes();
 });
---
+++ database/migrations/2019_03_30_151144_crear_tabla_detalle_person.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTablaDetallePerson extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_person', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('fecha_salida')->nullable();
+            $table->timestamp('fecha_ingreso')->nullable();
+            $table->integer('person_id')->unsigned();
+            $table->integer('sucursal_id')->unsigned();
+            $table->foreign('person_id')->references('id')->on('person')->onUpdate('restrict')->onDelete('restrict');
+            $table->foreign('sucursal_id')->references('id')->on('sucursal')->onUpdate('restrict')->onDelete('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalle_person');
+    }
+}
