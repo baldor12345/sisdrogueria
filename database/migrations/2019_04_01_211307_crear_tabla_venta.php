92ed3add48d4b8a7933a45e61b05b23c0394a641
driver5019 <azbing18@gmail.com>
2019-04-02 13:43:26 -0500

    baldor

--- database/migrations/2019_04_01_211307_crear_tabla_venta.php
+++ database/migrations/2019_04_01_211307_crear_tabla_venta.php
@@ -15,7 +15,18 @@ class CrearTablaVenta extends Migration
     {
         Schema::create('venta', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('cantidad')->nullable();
+            $table->decimal('precio_unitario',20, 2)->nullable();
+            $table->decimal('descuento',20, 2)->nullable();
+            $table->string('descripcion', 400)->nullable();
+            $table->timestamp('fecha_hora')->nullable();
+            $table->char('estado', 1)->nullable();// A=>abierto; C=>cerrado
+            $table->integer('producto_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('producto_id')->references('id')->on('producto')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('restrict')->onUpdate('restrict');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
