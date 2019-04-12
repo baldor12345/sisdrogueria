f8f2cdee03e3369159ca1d326561b11123e43721
alex_gastelo <alexgastelob@outlook.com>
2019-04-12 16:50:34 -0500

    GASTELO

--- resources/views/app/ventas/mant.blade.php
+++ resources/views/app/ventas/mant.blade.php
@@ -1,50 +1,61 @@
 <div id="divMensajeError{!! $entidad !!}"></div>
-{!! Form::model($sucursal, $formData) !!}	
-	{!! Form::hidden('listar', $listar, array('id' => 'listar')) !!}}
-	{!! Form::hidden('total', 0, array('id' => 'total')) !!}}
+{!! Form::model($venta, $formData) !!}	
+	{!! Form::hidden('listar', $listar, array('id' => 'listar')) !!}
+	{!! Form::hidden('total', 0, array('id' => 'total')) !!}
+	<div class="row card-box">
 
-	<div class="form-group col-6 col-md-6">
-		{!! Form::label('cboCliente', 'Cliente: ', array('class' => '')) !!}
-		{!! Form::select('cboCliente', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboCliente')) !!}
-	</div>
-	<div class="form-group col-6 col-md-6">
-		{!! Form::label('cboForma_pago', 'Forma de pago: ', array('class' => '')) !!}
-		{!! Form::select('cboForma_pago', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboForma_pago')) !!}
-	</div>
-	<div class="form-group col-6 col-md-6">
-		{!! Form::label('cboComprobante', 'Comprobante Pago: ', array('class' => '')) !!}
-		{!! Form::select('cboComprobante', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboComprobante')) !!}
-	</div>
-	<div class="form-group col-3 col-md-3">
-		{!! Form::label('cboProducto', 'Producto: ', array('class' => '')) !!}
-		{!! Form::select('cboProducto', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboProducto')) !!}
-	</div>
-	<div class="form-group col-3 col-md-3">
-		{!! Form::label('cboUnidad', 'Medida: ', array('class' => '')) !!}
-		{!! Form::select('cboUnidad', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboUnidad')) !!}
-	</div>
-	<div class="form-group col-3 col-md-3">
-		{!! Form::label('cantidad', 'Cantidad:', array('class' => 'control-label')) !!}
-		{!! Form::text('cantidad', null, array('class' => 'form-control input-xs', 'id' => 'cantidad', 'placeholder' => 'Cantidad')) !!}
-	</div>
-	<div class="form-group col-3 col-md-3">
-			{!! Form::button('<i class="fa fa-plus fa-lg"></i> Add', array('class' => 'btn btn-success btn-sm', 'id' => 'btnAgregar', 'onclick' => '')) !!}
-	</div>
-	<div class="table-responsive">
-		<table>
-			<thead>
-				<tr>
-					<th>Nombre</th>
-					<th>Cantidad</th>
-					<th>Precio Unitario</th>
-					<th>Total S/.</th>
-				</tr>
-			</thead>
-			<tbody id="tabla_productos">
+		<div class="form-group col-9 col-md-9">
+			{!! Form::label('cboCliente', 'Cliente: ', array('class' => '')) !!}
+			{!! Form::select('cboCliente', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboCliente')) !!}
+		</div>
+		<div class="form-group col-2 col-md-2" style="magin-left: 10px">
+				{!! Form::label('btnadd', 'Nuevo: ', array('class' => 'control-label col-12 col-md-12')) !!}
+			{!! Form::button('<i class="fa fa-plus fa-lg"></i> Registrar Nuevo', array('class' => 'btn btn-success btn-sm', 'id' => 'btnadd', 'onclick' => '')) !!}
+		</div>
+		<div class="form-group col-6 col-md-6">
+			{!! Form::label('cboForma_pago', 'Forma de pago: ', array('class' => '')) !!}
+			{!! Form::select('cboForma_pago', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboForma_pago')) !!}
+		</div>
+		<div class="form-group col-6 col-md-6"  style="magin-left: 10px">
+			{!! Form::label('cboComprobante', 'Comprobante Pago: ', array('class' => '')) !!}
+			{!! Form::select('cboComprobante', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboComprobante')) !!}
+		</div>
 
-			</tbody>
-			
-		</table>
+	</div>
+	<div class="row card-box">
+		<div class="form-group col-3 col-md-3">
+			{!! Form::label('cboProducto', 'Producto: ', array('class' => '')) !!}
+			{!! Form::select('cboProducto', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboProducto')) !!}
+		</div>
+		<div class="form-group col-3 col-md-3">
+			{!! Form::label('cboUnidad', 'Medida: ', array('class' => '')) !!}
+			{!! Form::select('cboUnidad', array('0'=>'Seleccione'), '0' , array('class' => 'form-control input-sm', 'id' => 'cboUnidad')) !!}
+		</div>
+		<div class="form-group col-3 col-md-3">
+			{!! Form::label('cantidad', 'Cantidad:', array('class' => 'control-label')) !!}
+			{!! Form::text('cantidad', null, array('class' => 'form-control input-xs', 'id' => 'cantidad', 'placeholder' => 'Cantidad')) !!}
+		</div>
+		<div class="form-group col-3 col-md-3">
+				{!! Form::button('<i class="fa fa-plus fa-lg"></i> Add', array('class' => 'btn btn-success btn-sm', 'id' => 'btnAgregar', 'onclick' => '')) !!}
+		</div>
+		<div class="form-group col-12 col-md-12">
+			<div class="table-responsive">
+				<table class="table table-bordered table-striped table-condensed table-hover">
+					<thead>
+						<tr>
+							<th>Nombre</th>
+							<th>Cantidad</th>
+							<th>Precio Unitario</th>
+							<th>Total S/.</th>
+						</tr>
+					</thead>
+					<tbody id="tabla_productos">
+
+					</tbody>
+					
+				</table>
+			</div>
+		</div>
 	</div>
 	
 	<div class="form-group">
@@ -57,7 +68,7 @@
 {!! Form::close() !!}
 <script type="text/javascript">
 $(document).ready(function() {
-	configurarAnchoModal('450');
+	configurarAnchoModal('750');
 	init(IDFORMMANTENIMIENTO+'{!! $entidad !!}', 'M', '{!! $entidad !!}');
 }); 
 </script>
